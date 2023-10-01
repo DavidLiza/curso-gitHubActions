734347ed99b1a6e4532bb4b9cad85ea9d9973f34
David Enrique Lizarazo Vesga <55042262+DavidLiza@users.noreply.github.com>
2023-09-30 20:56:24 -0500

    added Files for first task

---
+++ holaMundo.py
@@ -0,0 +1,8 @@
+import os 
+
+def mainFunction ():
+    username = os.getenv("USERNAME")
+    print ("Hola {} Desde un Github action ".format(username))
+
+if __name__ == "main":
+    mainFunction()
