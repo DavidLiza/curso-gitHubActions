80bef35877bffa43fa02c4053bb37a6d0de9745c
David Enrique Lizarazo Vesga <55042262+DavidLiza@users.noreply.github.com>
2023-10-06 08:57:00 -0500

    Again To test

--- holaMundo.py
+++ holaMundo.py
@@ -2,7 +2,7 @@ import os
 
 def mainFunction ():
     username = os.getenv("USERNAME")
-    print ("Hola {} Desde un Github action ".format(username))
+    print ("Hola Desde un Github action")
 
 if __name__ == "main":
     print ("Pos Hola desde aqui antes de que se dañe")
