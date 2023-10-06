8f00a907709acee5944b7c6a6820a442f3f0d43a
David Enrique Lizarazo Vesga <55042262+DavidLiza@users.noreply.github.com>
2023-10-06 09:01:50 -0500

    Otra modificacion mas

--- holaMundo.py
+++ holaMundo.py
@@ -1,9 +1,7 @@
-import os 
-
 def mainFunction ():
-    username = os.getenv("USERNAME")
     print ("Hola Desde un Github action")
 
+print ("Script corrido")
 if __name__ == "main":
     print ("Pos Hola desde aqui antes de que se dañe")
     mainFunction()
